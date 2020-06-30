8ec6360163103d4953ed520044fbc77cf46301c1
juanpbergues <32652663+juanpbergues@users.noreply.github.com>
2020-06-30 10:18:44 -0300

    Agregando titulo y boton de Add

--- rentnow-web/src/components/App/Complejos/Complejos.js
+++ rentnow-web/src/components/App/Complejos/Complejos.js
@@ -4,19 +4,37 @@ import {
   CardActionArea,
   CardContent,
   CardMedia,
+  Container
 } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import addImage from "../../../assets/img/add_circle_outline-black-24dp.svg"
- 
+import addImage from "../../../assets/img/more-image.png"
+
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+
+
+
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
     maxHeight: 232,
   },
+  container: {
+    marginTop: theme.spacing(10),
+    marginBottom: theme.spacing(15),
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
   media: {
     height: 140,
+  },
+  item: {
+    display: 'flex',
+    alignItems: 'center',
   },
   subrayado: {
     height: 5,
@@ -26,6 +44,20 @@ const useStyles = makeStyles((theme) => ({
     background:
       "rgb(255,191,0) linear-gradient(90deg, rgba(255,191,0,0.7517401392111369) 29%, rgba(255,255,191,1) 100%)",
   },
+  title: {
+    marginBottom: theme.spacing(4),
+  },
+  image: {
+    backgroundPosition: "center",
+    minHeight: "100px",
+    height: "100%",
+    width: "100%",
+  },
+  addButton: {
+    position: 'fixed',
+    bottom: theme.spacing(3),
+    right: theme.spacing(3),
+  }
 }));
 
 const complejos = [
@@ -42,48 +74,57 @@ const Complejos = () => {
 
   return (
     <>
-      <Grid container spacing={5}>
-        {complejos.map((complejo) => (
-          <Grid key={complejo.nombre} item xs={12} md={4}>
+      <Container className={classes.container}>
+        <Typography variant="h4" marked="center" className={classes.title} component="h2">
+          <b>TUS COMPLEJOS</b>
+          <span className={classes.subrayado} />
+        </Typography>
+        <Grid container spacing={5}>
+          {complejos.map((complejo) => (
+            <Grid key={complejo.nombre} item xs={12} md={4}>
+              <Card elevation={10} className={classes.root}>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={complejo.image}
+                    title={complejo.nombre}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {complejo.nombre}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      {complejo.direccion}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+          <Grid item xs={12} md={4}>
             <Card elevation={10} className={classes.root}>
               <CardActionArea>
                 <CardMedia
                   className={classes.media}
-                  image={complejo.image}
-                  title={complejo.nombre}
+                  image={addImage}
+                  title="Registrar un nuevo complejo"
                 />
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="h2">
-                    {complejo.nombre}
-                  </Typography>
+                    Nuevo Complejo
+                </Typography>
                   <Typography variant="body2" color="textSecondary">
-                    {complejo.direccion}
+                    Click aquí para registrar un nuevo complejo!
                   </Typography>
                 </CardContent>
               </CardActionArea>
             </Card>
           </Grid>
-        ))}
-        <Grid item xs={12} md={4}>
-          <Card elevation={10} className={classes.root}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image={addImage}
-                title="Nuevo complejo"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Nuevo complejo
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                    Click aquí para registrar un nuevo complejo!
-                  </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
         </Grid>
-      </Grid>
+      </Container>
+      <Fab color="primary" aria-label="add" title="Registrar un nuevo complejo" className={classes.addButton}>
+        <AddIcon />
+      </Fab>
     </>
   );
 };
