68fccf64908202a3839db061c1c3cd1e2c494bce
Facundo Villard <facuvillard@gmail.com>
2020-08-24 12:14:34 -0300

    Se agregan campos en formulario, falta funcionalidad para validar datos y cargar en Firebase

--- rentnow-web/src/components/App/Espacios/Espacios.js
+++ rentnow-web/src/components/App/Espacios/Espacios.js
@@ -1,5 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  CardContent,
+  CardMedia,
+  CardActionArea,
+  Card,
+  Typography,
+  Grid,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import LinkCustom from "components/utils/LinkCustom/LinkCustom";
+import * as Routes from "constants/routes";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import { useLocation } from "react-router-dom";
 
-export default function Espacios() {
-    return <h1>vista de espacios</h1>
+const useStyles = makeStyles((theme) => ({
+  addButton: {
+    position: "fixed",
+    bottom: theme.spacing(3),
+    right: theme.spacing(3),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+export default function Espacios(props) {
+  const [espacios, setEspacios] = useState(["1", "2", "3", "4", "5", "6"]);
+  const currentLocation = useLocation().pathname;
+  const classes = useStyles();
+  return (
+    <>
+      <Grid container spacing={6}>
+        {espacios.map((espacio) => (
+          <Grid item xs={4}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  width="140"
+                  image="https://i1.wp.com/www.parqueygrama.com/wp-content/uploads/2018/02/medidas-reglamentarias-para-canchas-de-futbol-5-6-7-8-9-11.jpg?w=1025&ssl=1"
+                  title="Cancha de futbol 1"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Best Cancha de Futbol Ever
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Cancha futból 5
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+
+      <LinkCustom to={currentLocation + "/registrar"}>
+        <Fab
+          color="primary"
+          aria-label="add"
+          title="Registrar espacios"
+          className={classes.addButton}
+          variant="extended"
+        >
+          <AddIcon className={classes.extendedIcon} />
+          Nuevo espacio
+        </Fab>
+      </LinkCustom>
+    </>
+  );
 }
--- rentnow-web/src/components/App/Espacios/RegisterEspacios.js
+++ rentnow-web/src/components/App/Espacios/RegisterEspacios.js
@@ -1,5 +1,170 @@
 import React from "react";
+import {
+  Typography,
+  Grid,
+  TextField,
+  MenuItem,
+  Button,
+} from "@material-ui/core";
+import { Formik, Form, Field } from "formik";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+
+const tiposEspacios = [
+  "Cancha Futbol",
+  "Cancha Basquet",
+  "Cancha Handball",
+  "Cancha Tenis",
+  "Cancha Paddle",
+  "Cancha Hockey",
+  "Quincho",
+  "Asador",
+];
+const tiposPisos = [
+  "Parqué",
+  "Cesped natural",
+  "Cesped sintetico",
+  "Cemento",
+  "Polvo de ladrillo",
+  "Tierra",
+];
+const infraEstructura = ["Abierta con luz", "Abierta sin luz", "Techada"];
+const estados = ["En reparación", "Disponible"];
 
 export default function RegisterEspacios() {
-  return <h1>Registrar Espacios</h1>;
+  return (
+    <Formik
+      initialValues={{
+        nombre: "",
+        tipoEspacio: "",
+        capacidad: "",
+        tipoPiso: "",
+        horaDesde: "",
+        horaHasta: "",
+        estado: "",
+        infraestructura: "",
+        descripcion: "",
+      }}
+    >
+      <Form>
+        <Grid container spacing={6}>
+          <Grid item xs={6}>
+            <Field name="nombre" label="Nombre*" fullWidth as={TextField} />
+          </Grid>
+          <Grid item xs={6}>
+            <Field
+              component={TextField}
+              select
+              fullWidth
+              label="Tipo de espacio*"
+              name="tipoEspacio"
+            >
+              {tiposEspacios.map((tipo) => (
+                <MenuItem key={tipo} value={tipo}>
+                  {tipo}
+                </MenuItem>
+              ))}
+            </Field>
+          </Grid>
+          <Grid item xs={6}>
+            <Field
+              name="capacidad"
+              label="Capacidad (personas)*"
+              fullWidth
+              as={TextField}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <Field
+              component={TextField}
+              select
+              fullWidth
+              label="Tipo de piso*"
+              name="tipoPiso"
+            >
+              {tiposPisos.map((tipo) => (
+                <MenuItem key={tipo} value={tipo}>
+                  {tipo}
+                </MenuItem>
+              ))}
+            </Field>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Field
+              component={TextField}
+              type="time"
+              label="Desde*"
+              defaultValue="07:30"
+              name="horaHasta"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Field
+              component={TextField}
+              type="time"
+              label="Hasta*"
+              defaultValue="20:00"
+              name="horaHasta"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <Field
+              component={TextField}
+              select
+              fullWidth
+              label="Estado*"
+              name="estado"
+            >
+              {estados.map((estado) => (
+                <MenuItem key={estado} value={estado}>
+                  {estado}
+                </MenuItem>
+              ))}
+            </Field>
+          </Grid>
+          <Grid item xs={6}>
+            <Field
+              component={TextField}
+              select
+              fullWidth
+              label="Infraestructura*"
+              name="infraestructura"
+            >
+              {estados.map((estado) => (
+                <MenuItem key={estado} value={estado}>
+                  {estado}
+                </MenuItem>
+              ))}
+            </Field>
+          </Grid>
+          <Grid item xs={6}>
+            <Field
+              component={TextField}
+              multiline
+              rows={3}
+              fullWidth
+              label="Descripción de prestaciones"
+              name="descripcion"
+            />
+          </Grid>
+          <Grid container direction="row" justify="center" alignItems="center">
+            <Button
+              startIcon={<ArrowBackIcon />}
+              variant="contained"
+              color="primary"
+            >
+              Volver
+            </Button>
+            <Button variant="contained" color="primary">
+              Guardar
+            </Button>
+            <Button variant="contained" color="primary">
+              Guardar y registrar
+            </Button>
+          </Grid>
+        </Grid>
+      </Form>
+    </Formik>
+  );
 }
