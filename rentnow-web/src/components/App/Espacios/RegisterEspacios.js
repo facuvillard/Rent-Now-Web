44c6d191855a47eda8c9b02999438c7297c2a8c7
Facundo Villard <facuvillard@gmail.com>
2020-08-24 16:16:52 -0300

    Se refina formulario, falta control y registrar en base de datos

--- rentnow-web/src/components/App/Espacios/RegisterEspacios.js
+++ rentnow-web/src/components/App/Espacios/RegisterEspacios.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-  Typography,
   Grid,
   TextField,
   MenuItem,
   Button,
+  CircularProgress,
 } from "@material-ui/core";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form } from "formik";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import { useParams } from "react-router-dom";
+import LinkCustom from "components/utils/LinkCustom/LinkCustom";
 
 const tiposEspacios = [
   "Cancha Futbol",
@@ -27,144 +29,217 @@ const tiposPisos = [
   "Polvo de ladrillo",
   "Tierra",
 ];
-const infraEstructura = ["Abierta con luz", "Abierta sin luz", "Techada"];
+const infraestructuras = ["Abierta con luz", "Abierta sin luz", "Techada"];
 const estados = ["En reparación", "Disponible"];
 
 export default function RegisterEspacios() {
+  const { idComplejo } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const registrarEspacio = (espacio) => {
+    console.log(espacio);
+
+    //TO DO: Llamar a Api y registrar espacio
+  };
+
   return (
-    <Formik
-      initialValues={{
-        nombre: "",
-        tipoEspacio: "",
-        capacidad: "",
-        tipoPiso: "",
-        horaDesde: "",
-        horaHasta: "",
-        estado: "",
-        infraestructura: "",
-        descripcion: "",
-      }}
-    >
-      <Form>
-        <Grid container spacing={6}>
-          <Grid item xs={6}>
-            <Field name="nombre" label="Nombre*" fullWidth as={TextField} />
-          </Grid>
-          <Grid item xs={6}>
-            <Field
-              component={TextField}
-              select
-              fullWidth
-              label="Tipo de espacio*"
-              name="tipoEspacio"
-            >
-              {tiposEspacios.map((tipo) => (
-                <MenuItem key={tipo} value={tipo}>
-                  {tipo}
-                </MenuItem>
-              ))}
-            </Field>
-          </Grid>
-          <Grid item xs={6}>
-            <Field
-              name="capacidad"
-              label="Capacidad (personas)*"
-              fullWidth
-              as={TextField}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <Field
-              component={TextField}
-              select
-              fullWidth
-              label="Tipo de piso*"
-              name="tipoPiso"
-            >
-              {tiposPisos.map((tipo) => (
-                <MenuItem key={tipo} value={tipo}>
-                  {tipo}
-                </MenuItem>
-              ))}
-            </Field>
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <Field
-              component={TextField}
-              type="time"
-              label="Desde*"
-              defaultValue="07:30"
-              name="horaHasta"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <Field
-              component={TextField}
-              type="time"
-              label="Hasta*"
-              defaultValue="20:00"
-              name="horaHasta"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <Field
-              component={TextField}
-              select
-              fullWidth
-              label="Estado*"
-              name="estado"
-            >
-              {estados.map((estado) => (
-                <MenuItem key={estado} value={estado}>
-                  {estado}
-                </MenuItem>
-              ))}
-            </Field>
-          </Grid>
-          <Grid item xs={6}>
-            <Field
-              component={TextField}
-              select
-              fullWidth
-              label="Infraestructura*"
-              name="infraestructura"
-            >
-              {estados.map((estado) => (
-                <MenuItem key={estado} value={estado}>
-                  {estado}
-                </MenuItem>
-              ))}
-            </Field>
-          </Grid>
-          <Grid item xs={6}>
-            <Field
-              component={TextField}
-              multiline
-              rows={3}
-              fullWidth
-              label="Descripción de prestaciones"
-              name="descripcion"
-            />
-          </Grid>
-          <Grid container direction="row" justify="center" alignItems="center">
-            <Button
-              startIcon={<ArrowBackIcon />}
-              variant="contained"
-              color="primary"
-            >
-              Volver
-            </Button>
-            <Button variant="contained" color="primary">
-              Guardar
-            </Button>
-            <Button variant="contained" color="primary">
-              Guardar y registrar
-            </Button>
+    <>
+      {isLoading ? (
+        <Grid container justify="center">
+          <Grid item>
+            <CircularProgress />
           </Grid>
         </Grid>
-      </Form>
-    </Formik>
+      ) : (
+        <Formik
+          initialValues={{
+            nombre: "",
+            tipoEspacio: "",
+            capacidad: "",
+            tipoPiso: "",
+            horaDesde: "07:00",
+            horaHasta: "22:00",
+            estado: "",
+            infraestructura: "",
+            descripcion: "",
+          }}
+          onSubmit={(values) => {
+            registrarEspacio({ ...values });
+          }}
+        >
+          {({ values, handleChange }) => (
+            <Form>
+              <Grid container spacing={6}>
+                <Grid item xs={6}>
+                  <TextField
+                    name="nombre"
+                    label="Nombre*"
+                    fullWidth
+                    value={values.Nombre}
+                    onChange={(e) => {
+                      handleChange(e);
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    select
+                    name="tipoEspacio"
+                    label="Tipo de espacio"
+                    fullWidth
+                    value={values.tipoEspacio}
+                    onChange={(e) => {
+                      handleChange(e);
+                    }}
+                  >
+                    {tiposEspacios.map((tipo) => (
+                      <MenuItem key={tipo} value={tipo}>
+                        {tipo}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    name="capacidad"
+                    type="number"
+                    label="Capacidad (personas)*"
+                    fullWidth
+                    values={values.capacidad}
+                    onChange={(e) => {
+                      handleChange(e);
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    name="tipoPiso"
+                    select
+                    fullWidth
+                    label="Tipo de piso*"
+                    value={values.tipoPiso}
+                    onChange={(e) => {
+                      handleChange(e);
+                    }}
+                  >
+                    {tiposPisos.map((tipo) => (
+                      <MenuItem key={tipo} value={tipo}>
+                        {tipo}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </Grid>
+                <Grid item xs={6} sm={3}>
+                  <TextField
+                    name="horaDesde"
+                    type="time"
+                    label="Hora Desde*"
+                    value={values.horaDesde}
+                    onChange={(e) => {
+                      handleChange(e);
+                    }}
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={6} sm={3}>
+                  <TextField
+                    name="horaHasta"
+                    type="time"
+                    label="Hora Hasta*"
+                    value={values.horaHasta}
+                    onChange={(e) => {
+                      handleChange(e);
+                    }}
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    name="estado"
+                    select
+                    fullWidth
+                    label="Estado*"
+                    value={values.estado}
+                    onChange={(e) => {
+                      handleChange(e);
+                    }}
+                  >
+                    {estados.map((estado) => (
+                      <MenuItem key={estado} value={estado}>
+                        {estado}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    name="infraestructura"
+                    select
+                    fullWidth
+                    label="Infraestructura*"
+                    value={values.infraestructura}
+                    onChange={(e) => {
+                      handleChange(e);
+                    }}
+                  >
+                    {infraestructuras.map((infra) => (
+                      <MenuItem key={infra} value={infra}>
+                        {infra}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    name="descripcion"
+                    multiline
+                    rows={3}
+                    fullWidth
+                    label="Descripción de prestaciones"
+                    value={values.descripcion}
+                    onChange={(e) => {
+                      handleChange(e);
+                    }}
+                  />
+                </Grid>
+                <Grid
+                  container
+                  direction="row"
+                  justify="center"
+                  alignItems="center"
+                >
+                  <LinkCustom to={"/app/complejos/" + idComplejo + "/espacios"}>
+                    <Button
+                      startIcon={<ArrowBackIcon />}
+                      variant="contained"
+                      color="primary"
+                      style={{ margin: "10px" }}
+                    >
+                      Volver
+                    </Button>
+                  </LinkCustom>
+                  <Button
+                    style={{ margin: "10px" }}
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                  >
+                    Registrar
+                  </Button>
+                  <Button
+                    style={{ margin: "10px" }}
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                  >
+                    Registrar y continuar
+                  </Button>
+                </Grid>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
+      )}
+    </>
   );
 }
