f9d8ca5e29586b4b736f1b2ac01e3faf7be8894f
juanpbergues <32652663+juanpbergues@users.noreply.github.com>
2020-08-29 12:06:54 -0300

    Desarrollando parte de Dar de Baja Complejo

--- rentnow-web/src/components/App/Complejos/EditComplejo/EditComplejos.js
+++ rentnow-web/src/components/App/Complejos/EditComplejo/EditComplejos.js
@@ -10,6 +10,7 @@ import BasicData from 'components/App/Complejos/EditComplejo/Sections/BasicData'
 import LinkCustom from "components/utils/LinkCustom/LinkCustom";
 import { useParams } from "react-router-dom"
 import ImageVisualizer from "components/App/Complejos/EditComplejo/Sections/ImageVisualizer"
+import * as Routes from "constants/routes"
 
 
 const useStyles = makeStyles((theme) => ({
@@ -29,11 +30,14 @@ const useStyles = makeStyles((theme) => ({
         borderTop: "1px solid #BDBDBD",
         marginLeft: theme.spacing(0),
         marginRight: theme.spacing(0),
-        marginBottom: theme.spacing(5),
+        marginBottom: theme.spacing(3),
         borderRadius: "0.5px",
     },
     circularProgress: {
         marginTop: theme.spacing(25),
+    },
+    paragraph: {
+        marginBottom: theme.spacing(2),
     },
 }));
 
@@ -137,6 +141,29 @@ const EditComplejos = () => {
                                 <Grid item xs={12}>
                                     < hr className={classes.divider} />
                                     <ImageVisualizer fotos={complejo.fotos} />
+                                </Grid>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                        <ExpansionPanel className={classes.expansionPanel} >
+                            <ExpansionPanelSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                            >
+                                <Typography variant="h5" className={classes.heading}>Solicitud de Baja de Complejo</Typography>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <Grid item xs={12}>
+                                    < hr className={classes.divider} />
+                                    <Typography className={classes.paragraph}>
+                                        Aquí podras solicitar la baja de este complejo. Si das de baja tu complejo, este pasará a estado <b>Deshabilitado</b> y no se podrá acceder ni efectuar reservas en el mismo.
+                                    </Typography>
+                                    <Typography className={classes.paragraph}>
+                                        Esta baja se concretará efectivamente entre 24 y 48 horas, y las reservas en curso para este complejo seran canceladas.
+                                    </Typography>
+                                    <Typography className={classes.paragraph}>
+                                        En el caso de quieras volver a habilitarlo, deberas comunicarte con el equipo de RentNow en el apartado de <b>Contactanos</b> de la <a href={Routes.LANDING}>pagina principal</a> solicitando que tu complejo sea habilitado nuevamente.
+                                    </Typography>
                                 </Grid>
                             </ExpansionPanelDetails>
                         </ExpansionPanel>
