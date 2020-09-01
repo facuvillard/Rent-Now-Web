35f47861b62b8c18a172fd4fbc7c7fb3ebeb31a1
Sebas Maagnaldii <testing@munidigital.com>
2020-09-01 04:12:34 -0300

    Se repara componente EditComplejo

--- rentnow-web/src/components/App/Complejos/EditComplejo/EditComplejos.js
+++ rentnow-web/src/components/App/Complejos/EditComplejo/EditComplejos.js
@@ -6,40 +6,38 @@ import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { getComplejosById } from "../../../../api/complejos";
-import BasicData from 'components/App/Complejos/EditComplejo/Sections/BasicData/BasicData'
+import BasicData from "components/App/Complejos/EditComplejo/Sections/BasicData/BasicData";
 import LinkCustom from "components/utils/LinkCustom/LinkCustom";
-import { useParams } from "react-router-dom"
-import ImageVisualizer from "components/App/Complejos/EditComplejo/Sections/Images/ImageVisualizer"
-import DeleteComplejo from "components/App/Complejos/EditComplejo/Sections/DeleteComplejoRequest/DeleteComplejo"
-
-
+import { useParams } from "react-router-dom";
+import ImageVisualizer from "components/App/Complejos/EditComplejo/Sections/Images/ImageVisualizer";
+import DeleteComplejo from "components/App/Complejos/EditComplejo/Sections/DeleteComplejoRequest/DeleteComplejo";
 
 const useStyles = makeStyles((theme) => ({
-    expansionPanel: {
-        width: '100%',
-        border: '1px solid rgba(0, 0, 0, .125)',
-        backgroundColor: '#FAFAFA',
-        marginBottom: theme.spacing(3),
-    },
-    heading: {
-        position: "center"
-    },
-    boton: {
-        marginTop: theme.spacing(3),
-    },
-    divider: {
-        borderTop: "1px solid #BDBDBD",
-        marginLeft: theme.spacing(0),
-        marginRight: theme.spacing(0),
-        marginBottom: theme.spacing(3),
-        borderRadius: "0.5px",
-    },
-    circularProgress: {
-        marginTop: theme.spacing(25),
-    },
-    paragraph: {
-        marginBottom: theme.spacing(2),
-    },
+  expansionPanel: {
+    width: "100%",
+    border: "1px solid rgba(0, 0, 0, .125)",
+    backgroundColor: "#FAFAFA",
+    marginBottom: theme.spacing(3),
+  },
+  heading: {
+    position: "center",
+  },
+  boton: {
+    marginTop: theme.spacing(3),
+  },
+  divider: {
+    borderTop: "1px solid #BDBDBD",
+    marginLeft: theme.spacing(0),
+    marginRight: theme.spacing(0),
+    marginBottom: theme.spacing(3),
+    borderRadius: "0.5px",
+  },
+  circularProgress: {
+    marginTop: theme.spacing(25),
+  },
+  paragraph: {
+    marginBottom: theme.spacing(2),
+  },
 }));
 
 const EditComplejos = () => {
@@ -69,122 +67,121 @@ const EditComplejos = () => {
         </Grid>
       ) : (
         <>
-            {isLoading ? (
-                <Grid container justify="center" className={classes.circularProgress}>
-                    <Grid item>
-                        <CircularProgress />
-                    </Grid>
-                </Grid>
-            ) : (
-                    <>
-                        <ExpansionPanel className={classes.expansionPanel} >
-                            <ExpansionPanelSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography variant="h5" className={classes.heading}>Datos Básicos</Typography>
-                            </ExpansionPanelSummary>
-                            <ExpansionPanelDetails>
-                                <Grid item xs={12}>
-                                    < hr className={classes.divider} />
-                                    <BasicData
-                                        complejo={{
-                                            nombre: complejo.nombre,
-                                            email: complejo.email,
-                                            telefono: complejo.telefono,
-                                            instagram: complejo.redes.instagram,
-                                            facebook: complejo.redes.facebook,
-                                            twitter: complejo.redes.twitter
-                                        }}
-                                    />
-                                </Grid>
-                            </ExpansionPanelDetails>
-                        </ExpansionPanel>
-                        <ExpansionPanel className={classes.expansionPanel} >
-                            <ExpansionPanelSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography variant="h5" className={classes.heading}>Ubicación</Typography>
-                            </ExpansionPanelSummary>
-                            <ExpansionPanelDetails>
-                                <Grid item xs={12}>
-                                    < hr className={classes.divider} />
-                                    <Typography>
-                                        Aqui van los datos de UBICACION
-                    </Typography>
-                                </Grid>
-                            </ExpansionPanelDetails>
-                        </ExpansionPanel>
-                        <ExpansionPanel className={classes.expansionPanel} >
-                            <ExpansionPanelSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography variant="h5" className={classes.heading}>Dias y Horarios</Typography>
-                            </ExpansionPanelSummary>
-                            <ExpansionPanelDetails>
-                                <Grid item xs={12}>
-                                    < hr className={classes.divider} />
-                                    <Typography>
-                                        Aqui van los datos de DIAS Y HORARIOS
-                    </Typography>
-                                </Grid>
-                            </ExpansionPanelDetails>
-                        </ExpansionPanel>
-                        <ExpansionPanel className={classes.expansionPanel} >
-                            <ExpansionPanelSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography variant="h5" className={classes.heading}>Fotos</Typography>
-                            </ExpansionPanelSummary>
-                            <ExpansionPanelDetails>
-                                <Grid item xs={12}>
-                                    < hr className={classes.divider} />
-                                    <ImageVisualizer fotos={complejo.fotos} />
-                                </Grid>
-                            </ExpansionPanelDetails>
-                        </ExpansionPanel>
-                        <ExpansionPanel className={classes.expansionPanel} >
-                            <ExpansionPanelSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography variant="h5" className={classes.heading}>Solicitud de Baja de Complejo</Typography>
-                            </ExpansionPanelSummary>
-                            <ExpansionPanelDetails>
-                                <Grid item xs={12}>
-                                    < hr className={classes.divider} />
-                                    <DeleteComplejo complejo={{
-                                        nombre: complejo.nombre,
-                                        email: complejo.email,
-                                        telefono: complejo.telefono,
-                                    }} />
-                                </Grid>
-                            </ExpansionPanelDetails>
-                        </ExpansionPanel>
-                        <Grid container spacing={10} justify="center" className={classes.boton}>
-                            <Grid item xs={2}>
-                                <LinkCustom to={`/app/complejos/${idComplejo}`}>
-                                    <Button
-                                        variant="contained"
-                                        fullWidth
-                                        color="secondary"
-                                    >
-                                        Volver
-                                    </Button>
-                                </LinkCustom>
-                            </Grid>
-                        </Grid>
-                    </>
-                )
-            }
+          <ExpansionPanel className={classes.expansionPanel}>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography variant="h5" className={classes.heading}>
+                Datos Básicos
+              </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Grid item xs={12}>
+                <hr className={classes.divider} />
+                <BasicData
+                  complejo={{
+                    nombre: complejo.nombre,
+                    email: complejo.email,
+                    telefono: complejo.telefono,
+                    instagram: complejo.redes.instagram,
+                    facebook: complejo.redes.facebook,
+                    twitter: complejo.redes.twitter,
+                  }}
+                />
+              </Grid>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel className={classes.expansionPanel}>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography variant="h5" className={classes.heading}>
+                Ubicación
+              </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Grid item xs={12}>
+                <hr className={classes.divider} />
+                <Typography>Aqui van los datos de UBICACION</Typography>
+              </Grid>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel className={classes.expansionPanel}>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography variant="h5" className={classes.heading}>
+                Dias y Horarios
+              </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Grid item xs={12}>
+                <hr className={classes.divider} />
+                <Typography>Aqui van los datos de DIAS Y HORARIOS</Typography>
+              </Grid>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel className={classes.expansionPanel}>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography variant="h5" className={classes.heading}>
+                Fotos
+              </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Grid item xs={12}>
+                <hr className={classes.divider} />
+                <ImageVisualizer fotos={complejo.fotos} />
+              </Grid>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel className={classes.expansionPanel}>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography variant="h5" className={classes.heading}>
+                Solicitud de Baja de Complejo
+              </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Grid item xs={12}>
+                <hr className={classes.divider} />
+                <DeleteComplejo
+                  complejo={{
+                    nombre: complejo.nombre,
+                    email: complejo.email,
+                    telefono: complejo.telefono,
+                  }}
+                />
+              </Grid>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <Grid
+            container
+            spacing={10}
+            justify="center"
+            className={classes.boton}
+          >
+            <Grid item xs={2}>
+              <LinkCustom to={`/app/complejos/${idComplejo}`}>
+                <Button variant="contained" fullWidth color="secondary">
+                  Volver
+                </Button>
+              </LinkCustom>
+            </Grid>
+          </Grid>
+        </>
       )}
     </>
   );
