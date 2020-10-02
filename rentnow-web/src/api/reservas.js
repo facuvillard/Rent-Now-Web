eacda0175258d565ab5836a6b5b5d64803b69cb4
Sebas Maagnaldii <testing@munidigital.com>
2020-10-02 16:34:10 -0300

    Se recuperan reservas desde BD y se actualizan en tiempo real

--- rentnow-web/src/api/reservas.js
+++ rentnow-web/src/api/reservas.js
@@ -2,8 +2,11 @@ import firebase from "firebase";
 import moment from "moment";
 export async function registerReservaApi(reserva) {
   const semana = moment(reserva.fechaInicio).week();
+  const mes = moment(reserva.fechaInicio).month();
   const año = moment(reserva.fechaInicio).year();
-  const fechaRegistro = new firebase.firestore.Timestamp.fromDate(moment().toDate())
+  const fechaRegistro = new firebase.firestore.Timestamp.fromDate(
+    moment().toDate()
+  );
 
   reserva.fechaInicio = new firebase.firestore.Timestamp.fromDate(
     reserva.fechaInicio
@@ -13,10 +16,10 @@ export async function registerReservaApi(reserva) {
   );
 
   try {
-    const result = await firebase
+    await firebase
       .firestore()
       .collection("reservas")
-      .add({ ...reserva, semana, año, fechaRegistro });
+      .add({ ...reserva, semana, mes, año, fechaRegistro });
 
     return { status: "OK", message: "Se registró la reserva con exito" };
   } catch (err) {
@@ -51,6 +54,43 @@ export async function getReservasByWeekAndEspacio(fecha, idEspacio) {
     });
 
     return reservas;
+  } catch (err) {
+    console.log(err);
+    return {
+      status: "ERROR",
+      message: "Se produjo un error al registrar la reserva",
+      error: err,
+    };
+  }
+}
+
+export async function getReservasSixWeeksAndEspacioRealTime(
+  fecha,
+  idEspacio,
+  runWhenChange
+) {
+  try {
+    const firstWeek = moment(fecha).startOf("month").week() - 1;
+    const lastWeek = moment(fecha).endOf("month").week() + 1;
+    const year = moment(fecha).year();
+
+    const result = await firebase
+      .firestore()
+      .collection("reservas")
+      .where("espacio.id", "==", idEspacio)
+      .where("semana", ">=", firstWeek)
+      .where("semana", "<=", lastWeek)
+      .where("año", "==", year)
+      .onSnapshot((querySnapshot) => {
+        let reservas = [];
+
+        querySnapshot.forEach((reservaDoc) => {
+          reservas.push({ ...reservaDoc.data(), id: reservaDoc.id });
+        });
+        runWhenChange(reservas);
+      });
+
+    return result;
   } catch (err) {
     console.log(err);
     return {
