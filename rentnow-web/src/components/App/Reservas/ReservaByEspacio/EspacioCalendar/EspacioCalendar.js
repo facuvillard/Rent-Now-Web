3903b39228342b7162bee001ffb2237ac3170239
juanpbergues <32652663+juanpbergues@users.noreply.github.com>
2020-10-17 20:36:00 -0300

    Agregando referencias a los estados

--- rentnow-web/src/components/App/Reservas/ReservaByEspacio/EspacioCalendar/EspacioCalendar.js
+++ rentnow-web/src/components/App/Reservas/ReservaByEspacio/EspacioCalendar/EspacioCalendar.js
@@ -5,7 +5,12 @@ import { getReservasSixWeeksAndEspacioRealTime, updateReservaStateAndPayment } f
 import UpdateReserva from "components/App/Reservas/ReservaByEspacio/UpdateReserva"
 import Dialog from "components/utils/Dialog/Dialog";
 import AlertCustom from "components/utils/AlertCustom/AlertCustom";
-import {colorsByEstado} from 'constants/reservas/constants'
+import { colorsByEstado } from 'constants/reservas/constants';
+import StateReferences from "components/App/Reservas/ReservaByEspacio/EspacioCalendar/StateReferences";
+import Popover from '@material-ui/core/Popover';
+import Button from '@material-ui/core/Button';
+import { Grid } from "@material-ui/core";
+import HelpOutline from '@material-ui/icons/HelpOutline'
 
 
 
@@ -58,9 +63,9 @@ const EspacioCalendar = ({ espacio }) => {
   const updateDialogHandler = (reserva) => {
     setDialogContent(
       <UpdateReserva
-      updateHandler={(values) => {
-        updateHandler(values);
-      }}
+        updateHandler={(values) => {
+          updateHandler(values);
+        }}
         reserva={reserva}
         text="Modificando Reserva"
         setOpen={setOpen}
@@ -92,11 +97,25 @@ const EspacioCalendar = ({ espacio }) => {
     });
   }
 
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleHelpClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleHelpClose = () => {
+    setAnchorEl(null);
+  };
+
+  const helpOpen = Boolean(anchorEl);
+  const id = helpOpen ? 'simple-popover' : undefined;
 
 
   return (
     <>
+
       <Calendar
+        popup={true}
         style={{
           fontFamily: "Segoe UI",
           backgroundColor: "#FAFAFA",
@@ -110,21 +129,21 @@ const EspacioCalendar = ({ espacio }) => {
         defaultDate={fecha}
         onNavigate={dateChange}
         events={reservas}
-        eventPropGetter= {function(event, start, end, isSelected) {
-          const estadoReserva = event.estados[event.estados.length -1].estado
+        eventPropGetter={function (event, start, end, isSelected) {
+          const estadoReserva = event.estados[event.estados.length - 1].estado
           var backgroundColor = colorsByEstado[estadoReserva];
           var style = {
-              backgroundColor: backgroundColor,
-              borderRadius: '3px',
-              opacity: 0.8,
-              color: 'black',
-              fontWeight: 'bold',
-              border: '1px solid black',
+            backgroundColor: backgroundColor,
+            borderRadius: '3px',
+            opacity: 0.8,
+            color: 'black',
+            fontWeight: 'bold',
+            border: '1px solid black',
           };
           return {
-              style: style
+            style: style
           };
-      }}
+        }}
         onSelectEvent={event => updateDialogHandler(event)}
         min={moment(fecha).hour(5).minutes(0).toDate()}
         formats={{
@@ -136,6 +155,40 @@ const EspacioCalendar = ({ espacio }) => {
           },
         }}
       />
+      <Grid container
+        direction="row"
+        justify="center"
+        alignItems="center">
+        <Grid item md={1}>
+          <Button
+            size="small"
+            aria-describedby={id}
+            variant="contained"
+            color="primary"
+            onClick={handleHelpClick}
+            startIcon={<HelpOutline />}
+            color='inherit'
+          >
+            Ayuda
+      </Button>
+          <Popover
+            id={id}
+            open={helpOpen}
+            anchorEl={anchorEl}
+            onClose={handleHelpClose}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'center',
+              horizontal: 'center',
+            }}
+          >
+            <StateReferences />
+          </Popover>
+        </Grid>
+      </Grid>
       <Dialog
         title="Modificar Reserva"
         open={open}
---
+++ rentnow-web/src/components/App/Reservas/ReservaByEspacio/EspacioCalendar/StateReferences.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import Stop from '@material-ui/icons/Stop';
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid';
+import { estados, colorsByEstado } from 'constants/reservas/constants'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: 300,
+    },
+}));
+
+const StateReferences = () => {
+    const classes = useStyles();
+    return (
+        <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+        >
+            <Grid item md={6}>
+                <Typography><u><b>REFERENCIAS</b></u></Typography>
+            </Grid>
+            <Grid item md={12}>
+                <List dense={true}>
+                    <ListItem>
+                        <ListItemIcon>
+                            <Stop style={{ color: colorsByEstado[estados.confirmada] }} />
+                        </ListItemIcon>
+                        <ListItemText primary={estados.confirmada} />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <Stop style={{ color: colorsByEstado[estados.enHorario] }} />
+                        </ListItemIcon>
+                        <ListItemText primary={estados.enHorario} />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <Stop style={{ color: colorsByEstado[estados.enCurso] }} />
+                        </ListItemIcon>
+                        <ListItemText primary={estados.enCurso} />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <Stop style={{ color: colorsByEstado[estados.finalizada] }} />
+                        </ListItemIcon>
+                        <ListItemText primary={estados.finalizada} />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <Stop style={{ color: colorsByEstado[estados.cancelada] }} />
+                        </ListItemIcon>
+                        <ListItemText primary={estados.cancelada} />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <Stop style={{ color: colorsByEstado[estados.sinConcurrencia] }} />
+                        </ListItemIcon>
+                        <ListItemText primary={estados.sinConcurrencia} />
+                    </ListItem>
+                </List>
+            </Grid>
+        </Grid>
+
+    )
+}
+
+export default StateReferences
