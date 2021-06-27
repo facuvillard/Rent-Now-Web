e056228ac41798c62d528762f692434f8196a77f
Facundo Villard <facuvillard@gmail.com>
2021-06-27 10:38:35 -0300

    Estructura de listado de valoraciones

---
+++ rentnow-web/src/components/App/Valoraciones/ValoracionesList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ValoracionesList() {
+    return (
+        <div>
+            Vista de valoraciones
+        </div>
+    )
+}
--- rentnow-web/src/constants/breadcrumbs.js
+++ rentnow-web/src/constants/breadcrumbs.js
@@ -1,90 +1,95 @@
-import * as Routes from "./routes";
+import * as Routes from './routes';
 
 export const APP_COMPLEJOS = [
-  {
-    nombre: "Complejos",
-    ruta: Routes.COMPLEJOS,
-  },
+	{
+		nombre: 'Complejos',
+		ruta: Routes.COMPLEJOS,
+	},
 ];
 
 export const APP_COMPLEJO = [
-  ...APP_COMPLEJOS,
-  {
-    nombre: "Home",
-    ruta: Routes.COMPLEJO,
-  },
+	...APP_COMPLEJOS,
+	{
+		nombre: 'Home',
+		ruta: Routes.COMPLEJO,
+	},
 ];
 
 export const APP_REGISTRAR_COMPLEJO = [
-  ...APP_COMPLEJOS,
-  {
-    nombre: "Nuevo Complejo",
-    ruta: Routes.REGISTRAR_COMPLEJO,
-  },
+	...APP_COMPLEJOS,
+	{
+		nombre: 'Nuevo Complejo',
+		ruta: Routes.REGISTRAR_COMPLEJO,
+	},
 ];
 
 export const APP_MODIFICAR_COMPLEJO = [
-  ...APP_COMPLEJO,
-  {
-    nombre: "Modificar Complejo",
-    ruta: Routes.MODIFICAR_COMPLEJO,
-  },
+	...APP_COMPLEJO,
+	{
+		nombre: 'Modificar Complejo',
+		ruta: Routes.MODIFICAR_COMPLEJO,
+	},
 ];
 
 export const ADMIN_USUARIOS = [
-  {
-    nombre: "Usuarios",
-    ruta: Routes.USUARIOS,
-  },
+	{
+		nombre: 'Usuarios',
+		ruta: Routes.USUARIOS,
+	},
 ];
 
 export const ADMIN_ESTADISTICAS = [
-  {
-    nombre: "Estadisticas",
-    ruta: Routes.RANKING_CONCURRENCIA,
-  },
+	{
+		nombre: 'Estadisticas',
+		ruta: Routes.RANKING_CONCURRENCIA,
+	},
 ];
 
 export const ADMIN_COMPLEJOS = [
-  {
-    nombre: "Administrar Complejos",
-    ruta: Routes.ADMIN_COMPLEJOS,
-  },
+	{
+		nombre: 'Administrar Complejos',
+		ruta: Routes.ADMIN_COMPLEJOS,
+	},
 ];
 
-export const APP_ESPACIOS = [
-  ...APP_COMPLEJOS,
-  { nombre: "Espacios", ruta: Routes.ESPACIOS },
-];
+export const APP_ESPACIOS = [...APP_COMPLEJOS, { nombre: 'Espacios', ruta: Routes.ESPACIOS }];
 
 export const APP_REGISTRAR_ESPACIOS = [
-  ...APP_ESPACIOS,
-  { nombre: "Registrar Espacios", ruta: Routes.REGISTRAR_ESPACIO },
+	...APP_ESPACIOS,
+	{ nombre: 'Registrar Espacios', ruta: Routes.REGISTRAR_ESPACIO },
 ];
 
 export const APP_MODIFICAR_ESPACIO = [
-  ...APP_ESPACIOS,
-  { nombre: "Modificar Espacios", ruta: Routes.MODIFICAR_ESPACIO },
+	...APP_ESPACIOS,
+	{ nombre: 'Modificar Espacios', ruta: Routes.MODIFICAR_ESPACIO },
 ];
 
 export const APP_COMPLEJO_CALENDARIO = [
-  ...APP_COMPLEJO,
-  { nombre: "Calendario", ruta: Routes.CALENDARIO },
+	...APP_COMPLEJO,
+	{ nombre: 'Calendario', ruta: Routes.CALENDARIO },
 ];
 
 export const APP_COMPLEJO_RESERVAS_LISTADO = [
-  ...APP_COMPLEJO,
-  { nombre: "Listado de Reservas", ruta: Routes.LISTADO_RESERVAS },
+	...APP_COMPLEJO,
+	{ nombre: 'Listado de Reservas', ruta: Routes.LISTADO_RESERVAS },
 ];
 
 export const APP_AYUDAS = [
-  {
-    nombre: "Ayuda",
-    ruta: Routes.AYUDA,
-  },
+	{
+		nombre: 'Ayuda',
+		ruta: Routes.AYUDA,
+	},
 ];
 
 export const APP_REPORTES_CONCURRENCIA = [
-  ...APP_COMPLEJO,
-  { nombre: "Concurrencia", ruta: Routes.REPORTES_CONCURRENCIA },
-  ];
+	...APP_COMPLEJO,
+	{ nombre: 'Concurrencia', ruta: Routes.REPORTES_CONCURRENCIA },
+];
+
+export const APP_COMPLEJO_VALORACIONES_LISTADO = [
+	...APP_COMPLEJO,
+	{
+		nombre: 'Listado valoraciones',
+		ruta: Routes.LISTADO_VALORACIONES,
+	},
+];
