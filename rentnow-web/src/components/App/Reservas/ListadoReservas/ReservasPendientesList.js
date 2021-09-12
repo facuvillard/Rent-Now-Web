8c75867074a95feb8453a34ec4872468209fe05b
facuvillard <facundo.villard@mercadolibre.com>
2021-09-12 11:03:39 -0300

    primera version lista de pendientes

--- rentnow-web/src/components/App/Reservas/ListadoReservas/ReservasPendientesList.js
+++ rentnow-web/src/components/App/Reservas/ListadoReservas/ReservasPendientesList.js
@@ -1,8 +1,50 @@
+import MaterialTable from "material-table";
 import React from "react";
 
-
 const ReservasPendientesList = props => {
-	return <span>Reservas pendientes</span>
+	return (
+		<MaterialTable
+			title="Reservas pendientes de aprobación"
+			data={[]}
+			columns={[
+				{ title: 'ESPACIO', field: 'espacio.descripcion' },
+				{ title: 'FECHA INICIO', field: 'fechaInicioString', type: 'datetime' },
+				{ title: 'FECHA FIN', field: 'fechaFinString', type: 'datetime' },
+				{ title: 'ESTADO', field: 'estado' },
+				{ title: 'APELLIDO', field: 'cliente.apellido' },
+				{ title: 'NOMBRE', field: 'cliente.nombre' },
+				{ title: 'TELEFONO', field: 'cliente.celular' },
+			]}
+			options={{
+				actionsColumnIndex: -1,
+				pageSize: 10,
+				pageSizeOptions: [10],
+				rowStyle: {
+					backgroundColor: '#FAFAFA',
+				},
+				headerStyle: {
+					backgroundColor: '#FFF4E5',
+				},
+				exportButton: true,
+			}}
+			localization={{
+				body: {emptyDataSourceMessage: '¡No existen reservas pendientes de aprobación!'},
+				toolbar: {
+					exportTitle: 'Exportar como Excel',
+					exportName: 'Exportar como Excel',
+					searchTooltip: 'Buscar',
+					searchPlaceholder: 'Buscar',
+				},
+				pagination: {
+					lastTooltip: 'Ultima Pagina',
+					firstTooltip: 'Primera Pagina',
+					nextTooltip: 'Siguiente Pagina',
+					previousTooltip: 'Pagina Anterior',
+					labelDisplayedRows: '{from}-{to} de {count}',
+				},
+			}}
+		/>
+	)
 }
 
 
