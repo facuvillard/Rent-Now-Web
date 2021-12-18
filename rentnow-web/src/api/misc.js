a83b6dcc4f4d59f5aaa77636451aa60c04937454
juanpbergues <32652663+juanpbergues@users.noreply.github.com>
2021-12-18 20:13:28 -0300

    Changing contacto object to html

--- rentnow-web/src/api/misc.js
+++ rentnow-web/src/api/misc.js
@@ -1,6 +1,19 @@
 import firebase from "firebase";
 
 
+export async function sendEmailWithHTMLApi(emailData) {
+    const  formatedEmaildata = {
+        recipient: emailData.destinatario,
+        subject: emailData.asunto,
+        body: emailData.contenido,
+        attatchments: emailData.adjuntos
+    }
+    const sendEmailFromFirebase = firebase.functions().httpsCallable('sendContactEmailHTML')
+    const result = await sendEmailFromFirebase(formatedEmaildata)
+    
+    return result.data;
+}
+
 export async function sendEmailApi(emailData) {
     const  formatedEmaildata = {
         recipient: emailData.destinatario,
